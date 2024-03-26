f79b57ff29f1f6601c0a79a0cb693002924d6367
junsang.cho@icubeon.com <junsang.cho@icubeon.com>
2024-03-26 16:01:03 +0900

    First 프로젝트 시작

---
+++ src/main/java/org/example/codebuild/CodeBuildApplication.java
@@ -0,0 +1,13 @@
+package org.example.codebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeBuildApplication.class, args);
+    }
+
+}
