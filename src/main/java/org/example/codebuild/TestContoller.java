7daa497cd0da05c9c2124973d496b45edad3a24f
junsang.cho@icubeon.com <junsang.cho@icubeon.com>
2024-04-09 16:27:18 +0900

    변경사항 테스트

--- src/main/java/org/example/codebuild/TestContoller.java
+++ src/main/java/org/example/codebuild/TestContoller.java
@@ -1,15 +1,18 @@
 package org.example.codebuild;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/")
 public class TestContoller {
 
     @GetMapping("/test")
     public String test(){
+        log.info("Test Complete !!");
         return "OK";
     }
 }
